6e22e1644d3d1b0df2adba57866a3823e19e9178
gutihernandez <canaykul11@gmail.com>
2024-06-26 13:15:53 +0200

    app_readyy

--- chatbot_qa_app.py
+++ chatbot_qa_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from openai import OpenAI
 import logging
 import logging.handlers
-
+import time
 
 log_url=st.secrets["papertrail_url"]
 log_port=st.secrets["papertrail_port"]
@@ -29,6 +29,8 @@ user_question = st.text_input("Kuran'la alakalı bir soru sorunuz:")
 
 if user_question:
 
+
+
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     max_tokens = 1000,
@@ -44,7 +46,7 @@ if user_question:
     st.write("Cevap:", completion.choices[0].message.content)
     aggregated_result = "--user_question: " + str(user_question)+ "  --answer: "+ str(completion.choices[0].message.content)
 
-
+    time.sleep(2)
     logger.info(aggregated_result)
     #logger.info(msg=type(completion.choices[0].message.content))
     #logger.info(type(aggregated_result))
