aa9ad52faeb881389a619a763f5b0752e5aa9a4a
gutihernandez <canaykul11@gmail.com>
2024-06-26 13:10:07 +0200

    app_readyy

--- chatbot_qa_app.py
+++ chatbot_qa_app.py
@@ -46,8 +46,8 @@ if user_question:
     st.write("Cevap:", completion.choices[0].message.content)
     aggregated_result = "--user_question: " + str(user_question)+ "  --answer: "+ str(completion.choices[0].message.content)
 
-    logger.info(msg=type(user_question))
-    logger.info(msg=type(completion.choices[0].message.content))
+    logger.info('test')
+    #logger.info(msg=type(completion.choices[0].message.content))
     #logger.info(type(aggregated_result))
     #logger.info(aggregated_result)
 else:
